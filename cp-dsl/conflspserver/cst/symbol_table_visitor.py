75aeadf3fb1eb6756c3d25e3f16d392132c749f7
Reiner Jung <reiner.jung@email.uni-kiel.de>
2024-06-09 16:52:57 +0200

    Minor fix.

--- cp-dsl/conflspserver/cst/symbol_table_visitor.py
+++ cp-dsl/conflspserver/cst/symbol_table_visitor.py
@@ -41,12 +41,8 @@ import os
 # NOTE: Method names starting with visit are required to look like this, as parts of the grammar
 # are named in that way
 
-<<<<<<< HEAD
-class ConfigurationCPVisitor(ConfigurationVisitor, Generic[T]):
-=======
 
-class SymbolTableVisitor(ConfigurationVisitor, Generic[T]):
->>>>>>> origin/main
+class ConfigurationCPVisitor(ConfigurationVisitor, Generic[T]):
     _symbol_table: SymbolTable
 
     _generator_selector: str
