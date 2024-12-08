0c062d24dac5c74ca7f2ae6e65f7f5cb52baff65
Reiner Jung <reiner.jung@email.uni-kiel.de>
2024-12-08 18:26:00 +0100

    Refactored files.

--- cp-dsl/test/generator/visitors/test_utils.py
+++ cp-dsl/test/generator/visitors/test_utils.py
@@ -15,15 +15,20 @@
 __author__ = "reiner"
 
 import unittest
-from generator.visitors.declaration_visitor import GeneratorDeclarationVisitor
 from model.declaration_model import DeclarationModel
 from antlr4 import InputStream, CommonTokenStream
 from antlr4.Token import CommonToken
 from antlr4 import TerminalNode
 from antlr4.tree.Tree import TerminalNodeImpl
 
+from generator.visitors.declaration_visitor import GeneratorDeclarationVisitor
+from generator.visitors.configuration_visitor import GeneratorConfigurationVisitor
+
 from dcllspserver.gen.python.Declaration.DeclarationLexer import DeclarationLexer
 from dcllspserver.gen.python.Declaration.DeclarationParser import DeclarationParser
+
+from conflspserver.gen.python.Configuration.ConfigurationLexer import ConfigurationLexer
+from conflspserver.gen.python.Configuration.ConfigurationParser import ConfigurationParser
 
 from common.logger import GeneratorLogger
 from common.configuration import CompileFlags
@@ -62,6 +67,37 @@ class AbstractTestGeneratorDeclarationVisitor(unittest.TestCase):
         lexer = DeclarationLexer(input_stream)
         stream = CommonTokenStream(lexer)
         parser = DeclarationParser(stream)
+
+        visitor.visit(parser.declarationModel())
+
+        return model
+
+class AbstractTestGeneratorConfigurationVisitor(unittest.TestCase):
+
+    logger = GeneratorLogger(CompileFlags.STRICT)
+
+    def make_visitor(self, type=None) -> GeneratorConfigurationVisitor:
+        model = DeclarationModel()
+        if type is not None:
+            model._types[type.name] = type
+        return GeneratorConfigurationVisitor(model, self.logger)
+
+    def set_terminal(self, value:str) -> TerminalNode:
+        return TerminalNodeImpl(self.set_token(value))
+
+    def set_token(self, value:str) -> CommonToken:
+        token = CommonToken()
+        token.text = value
+        return token
+
+    def parse_code(self, code:str, model: DeclarationModel) -> DeclarationModel:
+        visitor = GeneratorConfigurationVisitor(model, self.logger)
+
+        input_stream = TestStream(code)
+
+        lexer = ConfigurationLexer(input_stream)
+        stream = CommonTokenStream(lexer)
+        parser = ConfigurationParser(stream)
 
         visitor.visit(parser.declarationModel())
 
